fabf2ea6ea9481521151278e02a3e502425caf42
smithkhatri <smithkhatri9@gmail.com>
2026-08-04 10:50:35 -0400

    test

--- brain.py
+++ brain.py
@@ -49,7 +49,7 @@ class JanusBrain:
         p_no = self.pm_book.best_no_ask_idx
         
         # We check < 100 as a quick filter. If the raw prices cross 100, we check deep book.
-        if k_yes + p_no < 100:
+        if k_yes + p_no < 99: # <======================================================================================================== TEST FOR NOW
             allocs, profit, volume = self._walk_book('yes', 'no')
             if profit > 0 and volume >= 50:
                 self._execute_trade(allocs, profit, volume, "Kalshi YES / PM NO", "yes", "no", kalshi_ticker=self.kalshi_ticker, pm_ticker=self.pm_ticker)
@@ -61,7 +61,8 @@ class JanusBrain:
         k_no = self.kalshi_book.best_no_ask_idx
         p_yes = self.pm_book.best_yes_ask_idx
 
-        if k_no + p_yes < 100: # Maybe add a statement to check if volume is not zero? maybe
+        if k_no + p_yes < 99: # <======================================================================================================== TEST FOR NOW
+            # Maybe add a statement to check if volume is not zero? maybe
             allocs, profit, volume = self._walk_book('no', 'yes')
             if profit > 0 and volume >= 50:
                 self._execute_trade(allocs, profit, volume, "Kalshi NO / PM YES", "no", "yes", kalshi_ticker=self.kalshi_ticker, pm_ticker=self.pm_ticker)
